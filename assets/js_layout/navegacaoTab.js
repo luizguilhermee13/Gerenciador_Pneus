59a84a89e4d4861765c201e0b548a51e8bed837c
luizguilhermee13 <luizguidelila@gmail.com>
2026-08-21 22:26:55 -0300

    navegacaoTab sidebar e background menu ativo

--- assets/js_layout/navegacaoTab.js
+++ assets/js_layout/navegacaoTab.js
@@ -1,9 +1,9 @@
-let menu = document.querySelectorAll("#sub-menu li");
+//navegação por tabs - sub menus
+const submenu = document.querySelectorAll("#sub-menu li");
+
 const secaoSelecionada = document.querySelectorAll(
   "#containerPrincipal section",
 );
-
-console.log(menu);
 
 function selecionarTopico(item) {
   secaoSelecionada.forEach((content) => {
@@ -12,8 +12,34 @@ function selecionarTopico(item) {
   secaoSelecionada[item].classList.add("ativo");
 }
 
-menu.forEach((item, id) => {
+if (submenu[0] == true) {
+  submenu[0].classList.add("sub-menu-selecionado");
+}
+
+submenu.forEach((item, id) => {
   item.addEventListener("click", () => {
     selecionarTopico(id);
+
+    submenu.forEach((outroItem) => {
+      outroItem.classList.remove("sub-menu-selecionado");
+    });
+
+    item.classList.add("sub-menu-selecionado");
   });
 });
+
+//navegação por tabs - menus - sidebar
+const sidebar = document.querySelectorAll("#menu ul a");
+
+function pageSelecionada(link) {
+  let href = link.href;
+  let url = location.href;
+
+  if (url.includes(href)) {
+    const itemPai = link.parentElement;
+
+    itemPai.classList.add("active");
+  }
+}
+
+sidebar.forEach(pageSelecionada);
